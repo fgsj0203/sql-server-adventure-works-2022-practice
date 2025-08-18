a299485d076f95d466423117bcc1345c532eaf0a
Francisco Gomes <fgsj.dev@outlook.com>
2025-08-18 09:14:59 -0300

    Data head of file in exercise clausules WHERE and LIKE

---
+++ database-example/SQL-Adventure-Works-Where-and-Like.sql
@@ -0,0 +1,15 @@
+/*
+Author: Francisco Gomes da Silva Junior
+Date: 18/08/2025, in format Brazil
+Location: Recife - PE
+Version:1.0
+Level exercises: beginner
+Content exercises: WHERE / LIKE
+*/
+
+
+
+
+
+
+
